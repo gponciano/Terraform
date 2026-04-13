022685c563af91de764c8bd07ddabf39a4f61cfb
gponciano <gab_bash@hotmail.com>
2026-04-13 20:25:43 +1000

    Adding lobal and data block

--- VPC/c3-datasources-and-locals.tf
+++ VPC/c3-datasources-and-locals.tf
@@ -1,1 +1,13 @@
 # Contains AWS region data source and local values
+
+# Data block
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+# Locals Block
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0,3)
+  public_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
+  private_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
+}
